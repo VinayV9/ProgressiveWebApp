9eb80cb34d8d78373d1f6353ee93f5c2a664f869
VinayV9 <vinayveeren@gmail.com>
2018-04-29 10:47:47 +0530

    project initial setup done :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterialdesignModule } from './modules/materialdesign/materialdesign.module'
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
---
+++ src/app/modules/materialdesign/materialdesign.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import {
+      MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule, 
+      MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, 
+      MatDividerModule, MatExpansionModule, MatFormFieldModule, MatGridListModule, 
+      MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, 
+      MatOptionModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, 
+      MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSlideToggleModule, 
+      MatSliderModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, 
+      MatTabsModule, MatToolbarModule, MatTooltipModule
+} from '@angular/material';
+
+@NgModule({
+      imports: [MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule, 
+      MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, 
+      MatDividerModule, MatExpansionModule, MatFormFieldModule, MatGridListModule, 
+      MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, 
+      MatOptionModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, 
+      MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSlideToggleModule, 
+      MatSliderModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, 
+      MatTabsModule, MatToolbarModule, MatTooltipModule ], 
+      exports: [MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule, 
+      MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, 
+      MatDividerModule, MatExpansionModule, MatFormFieldModule, MatGridListModule, 
+      MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, 
+      MatOptionModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, 
+      MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSlideToggleModule, 
+      MatSliderModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, 
+      MatTabsModule, MatToolbarModule, MatTooltipModule], 
+      declarations: []
+})
+export class MaterialdesignModule { }
