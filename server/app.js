60be7362868fdad7875a9bb2160181b1d9003fd3
VinayV9 <vinayveeren@gmail.com>
2018-04-30 23:28:28 +0530

    backend added auth feature

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// server client files
+app.use(express.static(__dirname + "/../dist"))
+
+app.get('/', (req, res)=>{
+   res.sendFile('/../dist/index.html')
+})
+
+app.get('*', (req, res) => {
+  res.redirect('/')
+})
+
+//routes
+app.use('/auth', require('./controllers/users.controller'))
+
+module.exports = app
